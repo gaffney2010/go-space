80097841f6e81cb7c96cbdad000d3afaab1c0881
T.J. Gaffney <gaffney.tj@gmail.com>
2021-06-27 12:31:31 -0500

    Make Point accept SGF format

--- board.py
+++ board.py
@@ -10,19 +10,49 @@ import attr
 SIZE = 9  # This will be fixed for us
 
 
+class PointFormatError(Exception):
+    pass
+
+
 @attr.s(frozen=True)
 class Point(object):
-    # By convention start counting from bottom-left
+    # By convention start counting from top-left
     row = attr.ib()  # Zero-indexed
     col = attr.ib()  # Zero-indexed
 
     @classmethod
     def fromLabel(cls, label: str) -> "Point":
-        """Returns a Point from an 'A1'-style human-label."""
-        letter, number = label[0].upper(), int(label[1:])
-        col = ord(letter) - ord("A")
-        row = number - 1  # To zero-index
-        return Point(row=row, col=col)
+        """Returns a Point from a label
+
+        Arguments:
+            label: May be in the format "A1" where "A" is the left-most column
+                and "1" is the bottom-most row.  Or in the SGF format "aa" where
+                the first letter is the left-most column, and the second letter
+                is the top-most row.
+
+        Returns:
+            A Point class with the specified label.
+        """
+        first, second = label[0], label[1:]
+        try:
+            format = "A1"
+            number = int(second)
+        except:
+            format = "SGF"
+
+        if format == "A1":
+            letter = first.upper()
+            col = ord(letter) - ord("A")
+            # Start from bottom with number=1 maps to SIZE-1
+            row = SIZE - number
+            return Point(row=row, col=col)
+        if format == "SGF":
+            first = first.lower()
+            second = second.lower()
+            col = ord(first) - ord("a")
+            row = ord(second) - ord("a")
+            return Point(row=row, col=col)
+        raise PointFormatError("This should never happen")
 
 
 def all_points() -> Iterator[Point]:
@@ -69,7 +99,7 @@ def boardFromJson(json_str: str) -> Board:
     Arguments:
         json_str: A json
 
-    returns:
+    Returns:
         A board that has only the specified black and white pieces.
     """
     parsed = json.loads(json_str)
